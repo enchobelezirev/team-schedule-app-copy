8d34c74940b1d02d3384e4c988918388b5332515
Encho Belezirev <encho.belezirev@lights.digital>
2022-01-11 17:26:47 +0200

    Remove CORS

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
 import json
 
 from shift_app.shift_generator import ShiftGenerator
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 shiftGenerator = ShiftGenerator()
 
 @app.route("/")
-@cross_origin()
 def hello():
     return json.dumps('Hi from TeamSchedule AI!')
 
 
 @app.route("/generate", methods=["POST"])
-@cross_origin()
 def generate():
 
     #TODO: Add real
@@ -26,7 +21,3 @@ def generate():
     # return json.dumps(employees[0].toJson())
 
     return json.dumps(request.json)
-
-if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    run_simple('localhost', 5000, app)
