55ca28eec1d4a1c17d45d3c64ee0eb9f55180969
Kristiyan Vachev <christian.vachev@lights.digital>
2022-01-13 09:12:29 +0200

    fixed bug with january being month zero

--- src/modules/api_mapper.py
+++ src/modules/api_mapper.py
@@ -6,7 +6,8 @@ from src.models.Shift import Shift
 
 def map_request_to_classes(requestJson: str) -> Schedule:
     managerId = requestJson['ManagerId']
-    weekStart = datetime(requestJson['MondayOfWeekToGenerateFor']['year'], requestJson['MondayOfWeekToGenerateFor']['month'], requestJson['MondayOfWeekToGenerateFor']['date'])
+    #The month from the input is in range [0, 11]. E.g. 0: January, 11: December.
+    weekStart = datetime(requestJson['MondayOfWeekToGenerateFor']['year'], requestJson['MondayOfWeekToGenerateFor']['month'] + 1, requestJson['MondayOfWeekToGenerateFor']['date'])
     
     employees = []
     for employee in requestJson['Employees']:
@@ -15,8 +16,8 @@ def map_request_to_classes(requestJson: str) -> Schedule:
         
         currShifts = []
         for shift in employee['PastShifts']:
-            currShiftStart = datetime(shift['startDate']['year'], shift['startDate']['month'], shift['startDate']['date'], shift['startTime']['hours'], shift['startTime']['minutes'])
-            currShiftEnd = datetime(shift['endDate']['year'], shift['endDate']['month'], shift['endDate']['date'], shift['endTime']['hours'], shift['endTime']['minutes'])
+            currShiftStart = datetime(shift['startDate']['year'], shift['startDate']['month'] + 1, shift['startDate']['date'], shift['startTime']['hours'], shift['startTime']['minutes'])
+            currShiftEnd = datetime(shift['endDate']['year'], shift['startDate']['month'] + 1, shift['endDate']['date'], shift['endTime']['hours'], shift['endTime']['minutes'])
             currShiftDuration = shift['duration']
             
             newShift = Shift(currShiftStart, currShiftEnd, currShiftDuration)
